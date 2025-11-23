134d5b70559a0de499616bb9642a933c1203cafb
Priyanka Daivagna <priyankadaivagna1615@gmail.com>
2025-11-23 15:47:13 +0530

    new changes

--- server/server.js
+++ server/server.js
@@ -18,4 +18,5 @@ app.get("/api/meetups/:id", (req, res) => {
   meetup ? res.json(meetup) : res.status(404).json({ message: "Not found" });
 });
 
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
