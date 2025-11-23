f550a9780bfe57faffb28df4a7306675a73e3863
Priyanka Daivagna <priyankadaivagna1615@gmail.com>
2025-11-23 16:01:10 +0530

    Updated api.js for Render Deployment

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,10 @@
-const BASE = "http://localhost:5000";
+// Use Render backend URL in production, localhost in development
+const BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 export const getMeetups = () =>
-  fetch(`${BASE}/api/meetups`).then((res) => res.json());
+    fetch(`${BASE}/api/meetups`).then((res) => res.json());
 
 export const getMeetup = (id) =>
-  fetch(`${BASE}/api/meetups/${id}`).then((res) => res.json());
+    fetch(`${BASE}/api/meetups/${id}`).then((res) => res.json());
 
 export const materialUrl = (file) => `${BASE}/materials/${file}`;
